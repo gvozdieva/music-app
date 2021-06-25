ee1dc40af910198816bab5710eb5c54f87d6abc5
Anastasiia Hvozdieva <AHvozdieva@lan.ad>
2021-06-25 16:54:32 +0300

    fixed song model

--- src/app/components/songs/song.model.ts
+++ src/app/components/songs/song.model.ts
@@ -1,13 +1,3 @@
 export class SongModel {
-  public name: string;
-  public url: string;
-  public author: string;
-  public time: string;
-
-  constructor (name: string, url: string, author: string,time: string) {
-    this.name = name;
-    this.url = url;
-    this.author = author;
-    this.time = time;
-  }
+  constructor (public name: string, public url: string, public author: string, public time: string, public newSong: boolean) {}
 }
--- src/app/components/songs/songs-list/songs-list.component.ts
+++ src/app/components/songs/songs-list/songs-list.component.ts
@@ -12,23 +12,28 @@ export class SongsListComponent {
     new SongModel('You',
       'https://www.youtube.com/watch?v=Zp1okMSCXEM',
       'Mk.gee',
-      '5:10'),
+      '5:10',
+      true),
     new SongModel('Groovejet (If This Ain\'t Love)',
       'https://www.youtube.com/watch?v=npJTeRjRX-8',
       'DJ Spiller feat. Sophie Ellis-Bextor',
-      '4:07'),
+      '4:07',
+      true),
     new SongModel('Enjoy your life',
       'https://www.youtube.com/watch?v=_6kgC-3s7Xg',
       'Oby Onyioha',
-      '5:57'),
+      '5:57',
+      false),
     new SongModel('Constant Surprises',
       'https://www.youtube.com/watch?v=F3ZE1w6dLUs',
       'Little Dragon',
-      '3:11'),
+      '3:11',
+      false),
     new SongModel('Molasses',
       'https://www.youtube.com/watch?v=2Ynj2lBHV_0',
       'Hiatus Kaiyote',
-      '4:52')
+      '4:52',
+      false)
   ]
   constructor() { }
 
