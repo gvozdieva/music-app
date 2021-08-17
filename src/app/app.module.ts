faaf5964768b2240111f86a56cf4abf7013cb932
AHvozdieva <nastiagvozdieva@gmail.com>
2021-08-17 15:09:38 +0300

    adding form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { AppRoutingModule } from './app-routing.module';
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { SongsListComponent } from './components/songs-list/songs-list.component';
 import { SongItemComponent } from './components/songs-list/song-item/song-item.component';
 import { ErrorComponent } from './components/error/error.component';
@@ -22,7 +22,8 @@ import { SongFormComponent } from './components/song-form/song-form.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
