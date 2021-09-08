60146b63ea7d53ad30585424f69d337ab25adcd3
Anastasiia Gvozdieva <nastiagvozdieva@gmail.com>
2021-09-09 01:43:39 +0300

    adding login form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,14 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
   submitted = false
 
   onSubmit() {
     this.submitted = true
-    console.log(this.submitted)
+
   }
 
 }
