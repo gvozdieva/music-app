2625f424bf38764971ef5ebb5c2254845de99ad6
Anastasiia Gvozdieva <nastiagvozdieva@gmail.com>
2021-09-17 23:54:50 +0300

    adding sign up form

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,17 +1,31 @@
-import { Component } from '@angular/core';
-import {NgForm} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
+  form!: FormGroup
 
-  onSignUp(form: NgForm) {
+  ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.email
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(4)
+      ])
+    })
+  }
+
+  /*onSignUp(form: NgForm) {
     const username = form.value.username
     const password = form.value.password
     console.log(username, password)
-  }
+  }*/
 
 }
