c420d6d064983203d3d5d9a40672941c6e9bdc05
AHvozdieva <nastiagvozdieva@gmail.com>
2021-09-28 14:42:48 +0300

    something

---
+++ projects/test-lib/src/lib/list/list/list.component.ts
@@ -0,0 +1,21 @@
+import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'tl-list',
+  template: `
+    <ul>
+      <li *ngFor="let item of list">{{item}}</li>
+    </ul>`,
+  styleUrls: ['./list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ListComponent implements OnInit {
+  @Input() list!: string[];
+
+  constructor(
+  ) {
+  }
+
+  ngOnInit() {
+  }
+}
